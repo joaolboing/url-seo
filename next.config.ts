d3e4cbf4cb04610c9094acb60b1d90cff648dc9d
João Boing <joao.boing@conectra.com.br>
2025-05-09 09:11:33 -0300

    initial commit with gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: isProd ? "/url-seo" : "",
-  trailingSlash: true,
-  assetPrefix: isProd ? "/url-seo" : "",
+  basePath: "/url-seo",
+  output: "export",
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
